d08529012026d5874ab1502863c5d61b0e161825
Peerapon-Sarikan <68011921@kmitl.ac.th>
2025-07-22 11:57:50 +0000

    week 4 lab

--- lab-4/ex03.c
+++ lab-4/ex03.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 int main() 
 {
-    int number;
-    const char *range_status[] = {"out of range", ""};
-    const char *parity[] = {"even", "odd"};
-    printf("Enter a number: ");
-    scanf("%d", &number);
+int number;
+const char *range_status[] = {"out of range", ""};
+const char *parity[] = {"even", "odd"};
+printf("Enter a number: ");
+scanf("%d", &number);
 
-    printf("%d is %s\n", number,
-           (number >= 1 && number <= 100)
-              ?  parity[number % 2]
-               : range_status[0]);
-
-    return 0;
+printf("%d is %s\n", number,
+(number >= 1 && number <= 100)
+?  parity[number % 2]
+: range_status[0]);
+ return 0;
 }
 
 
